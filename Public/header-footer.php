00b856219ca181767da296eb48ff799a865ee3e4
Abdurrahman98-web <135432850+Abdurrahman98-web@users.noreply.github.com>
2024-11-24 03:01:00 +0400

    Update header-footer.php

--- Public/header-footer.php
+++ Public/header-footer.php
@@ -23,7 +23,7 @@
                     <li class="nav-item"><a class="nav-link" href="./index.php">Accueil</a></li>
                     <li class="nav-item"><a class="nav-link" href="./habitats.php">Habitats</a></li>
                     <li class="nav-item"><a class="nav-link" href="./services.php">Services</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./ login.php">Connexion</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./login.php">Connexion</a></li>
                 </ul>
                 <a href="./contact.php" class="btn btn-outline-success ms-3 p-2">Contact</a>
             </div>
